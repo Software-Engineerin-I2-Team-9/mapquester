56b96e4568355cd74dd751b2b1fe994ee5c301c1
Yu Shi Jie <yshijie1999@gmail.com>
2024-11-26 18:20:15 -0500

    fixed

--- mapquester_backend/mapView/apps.py
+++ mapquester_backend/mapView/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MapviewConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "mapView"
+    name = "mapview"
--- mapquester_backend/mapquester/settings.py
+++ mapquester_backend/mapquester/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "users",
     "pois",
-    "mapView",
+    "mapview",
     "filters",
     "corsheaders",
     "rest_framework",
