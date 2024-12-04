3f684aeb488b7c95f704e404d67372930dec52d0
Shubham Rastogi <shu.rastogi337@gmail.com>
2024-12-04 12:49:40 -0500

    fix: fixed failing CI

--- mapquester_backend/mapquester/settings.py
+++ mapquester_backend/mapquester/settings.py
@@ -104,15 +104,15 @@ if "test" in sys.argv:
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
-elif 'RDS_DB_NAME' in os.environ:
+elif "RDS_DB_NAME" in os.environ:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": os.environ["RDS_DB_NAME"],
+            "USER": os.environ["RDS_USERNAME"],
+            "PASSWORD": os.environ["RDS_PASSWORD"],
+            "HOST": os.environ["RDS_HOSTNAME"],
+            "PORT": os.environ["RDS_PORT"],
         }
     }
 else:
