514c7fe188a25a232b42f82d5e38f317565d0be7
Shubham Rastogi <shu.rastogi337@gmail.com>
2024-12-03 23:55:36 -0500

    setting up production deployment

--- mapquester_backend/health/views.py
+++ mapquester_backend/health/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 @api_view(['GET'])
-def root_view(request):
+def health(request):
     return Response({
         "status": "ok",
         "message": "MapQuester API is running"
