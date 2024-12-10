dd021720429c07683ddc13b757d272a6ee823eec
Shubham Rastogi <shu.rastogi337@gmail.com>
2024-12-10 03:25:23 -0500

    fix: fixed cors for production

--- mapquester_backend/mapquester/settings.py
+++ mapquester_backend/mapquester/settings.py
@@ -72,6 +72,29 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",  # Next.js app running locally
+    "https://api.mapquester.website",
+]
+
+# Detailed CORS settings
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 ]
 
 ROOT_URLCONF = "mapquester.urls"
